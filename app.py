b44128a3ddef7b8a0015022fb0646632b847a3b8
jlexie <x-bit89@mail.ru>
2021-10-22 14:13:13 +0300

    lskdfjskld

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello, World 1'
+	return 'Hello, World. This is python app.'
 
 if __name__ == '__main__':
 	app.run(debug=True,host='0.0.0.0')
